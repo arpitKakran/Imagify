88654ca7f855081b122c034596fd9708f7333046
arpitKakran <arpitinsan77@gmail.com>
2025-08-27 17:46:12 +0530

    Update testimonials data with correct profile images and names

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -57,21 +57,21 @@ export const stepsData = [
 
 export const testimonialsData = [
     {
-        image:review2,
+        image:profile_img_2,
         name:'Ashish Gautam',
         role:'Full Stack Developer',
         stars:5,
         text:`I've been using this site for nearly two years, primarily for my projects, and it has been incredibly user-friendly, making my work much easier.`
     },
     {
-        image:review3,
-        name:'Kushgar Sharma',
+        image:profile_img_1,
+        name:'Kushagar Sharma',
         role:'ML Engineer',
         stars:4,
         text:`I have always struggled to find some stock images for my projects and I recently found this tool and its been a life saver since then`
     },
     {
-        image:review1,
+        image:profile_img_1,
         name:'Khushi Chahal',
         role:'Graphic Designer',
         stars:5,
